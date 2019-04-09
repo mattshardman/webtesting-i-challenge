0d481d4b836662ba0d0273b67c134733c21dbbe6
mattshardman <mattshardman@gmail.com>
2019-04-09 07:59:02 +0100

    add repari function test

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,7 +1,11 @@
 const enhancer = require('./enhancer.js');
 // test away!
 describe('enhancer functions', () => {
-
+    describe('repair function', () => {
+        it('Should return durability to full', () => {
+            expect(enhancer.repair().durability).toBe(100);
+        });    
+    });
    
 
 });
