cc09ca88e200d9bce63c54810194f48064ae7cad
mattshardman <mattshardman@gmail.com>
2019-04-09 07:59:47 +0100

    add succeed function tests

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -7,5 +7,17 @@ describe('enhancer functions', () => {
         });    
     });
    
+    describe('succeed function', () => {
+        it('return 20 for enhancement, if enhancement is already 20', () => {
+            expect(enhancer.succeed({ enhancement: 20 }).enhancement).toBe(20);
+
+        });
+
+        it('to return previous enhancement value + 1', () => {
+            expect(enhancer.succeed({ enhancement: 16 }).enhancement).toBe(17);
+            expect(enhancer.succeed({ enhancement: 13 }).enhancement).toBe(14);
+            expect(enhancer.succeed({ enhancement: 16 }).enhancement).not.toBe(16)
+        });
+    });
 
 });
